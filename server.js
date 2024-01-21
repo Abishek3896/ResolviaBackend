8b4ffea92960f90ca4a713d765abd44460395387
punyakriti5 <83115236+punyakriti5@users.noreply.github.com>
2024-01-21 14:53:25 +0530

    db connection

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url)
+  .then(() => {
+    console.log("Connected to the mongoDB!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the mongoDB!", err);
+    process.exit();
+  });
+
+app.get("/", (req, res) => {
   res.send('Resolvia Backend');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
+//require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
+
