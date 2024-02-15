fe45b2308e0e3f2b7c8c21f8d2e0de9dca937860
Abishek sridhar <abishekcs3896@gmail.com>
2024-02-15 22:08:54 +0530

    'cookies corrected'

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-const User = require("../models/user.model.js");
-const bcryptjs = require("bcryptjs");
-const error = require("../middleware/error.js");
-const jwt = require("jsonwebtoken");
+const User = require('../models/user.model.js');
+const bcryptjs = require('bcryptjs');
+const error = require('../middleware/error.js');
+const jwt = require('jsonwebtoken');
 const signup = async (req, res, next) => {
   const { username, email, mobileNum, password } = req.body;
   console.log(req.body);
@@ -10,11 +10,11 @@ const signup = async (req, res, next) => {
     !username ||
     !email ||
     !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
+    username === '' ||
+    email === '' ||
+    password === ''
   ) {
-    next(error(400, "All fields are required"));
+    next(error(400, 'All fields are required'));
   }
 
   const salt = bcryptjs.genSaltSync(10);
@@ -28,7 +28,7 @@ const signup = async (req, res, next) => {
   });
   try {
     await newUser.save();
-    res.json("SignUp successfull");
+    res.json('SignUp successfull');
   } catch (error) {
     next(error);
   }
@@ -37,17 +37,17 @@ const signup = async (req, res, next) => {
 const signin = async (req, res, next) => {
   const { username, password } = req.body;
   //console.log(username, password);
-  if (!username || !password || username === "" || password === "") {
-    next(error(400, "All fields are required"));
+  if (!username || !password || username === '' || password === '') {
+    next(error(400, 'All fields are required'));
   }
   try {
     const validUser = await User.findOne({ username });
     if (!validUser) {
-      return next(error(404, "User Not Valid"));
+      return next(error(404, 'User Not Valid'));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(error(400, "Invalid Password"));
+      return next(error(400, 'Invalid Password'));
     }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
@@ -55,10 +55,9 @@ const signin = async (req, res, next) => {
 
     res
       .status(200)
-      .cookie("access_token", token, {
+      .cookie('access_token', token, {
         httpOnly: true,
-        secure: true,
-        sameSite: "None",
+        domain: process.env.AUTH_COOKIE_DOMAIN,
       })
       .json(rest);
   } catch (error) {
@@ -79,11 +78,8 @@ const google = async (req, res, next) => {
       // HttpOnly (recommended for auth tokens): Prevents JavaScript access to the cookie, mitigating the risk of client-side script attacks.
       res
         .status(200)
-        .cookie("access_token", token, {
-          path: '/',
-      sameSite: 'None',
-      secure: true,
-      httpOnly: false,
+        .cookie('access_token', token, {
+          httpOnly: true,
           domain: process.env.AUTH_COOKIE_DOMAIN,
         })
         .json(rest);
@@ -94,7 +90,7 @@ const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          name.toLowerCase().split(" ").join("") +
+          name.toLowerCase().split(' ').join('') +
           Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
@@ -105,12 +101,8 @@ const google = async (req, res, next) => {
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
-        .cookie("access_token", token, {
-          path: '/',
-          sameSite: 'None',
-          secure: true,
-          httpOnly: false,
-
+        .cookie('access_token', token, {
+          httpOnly: true,
           domain: process.env.AUTH_COOKIE_DOMAIN,
         })
         .json(rest);
@@ -126,14 +118,14 @@ const refreshToken = async (req, res, next) => {
   try {
     const validEmail = await User.findOne({ email });
     if (!validEmail) {
-      return next(error(404, "User Not Valid"));
+      return next(error(404, 'User Not Valid'));
     }
     const validUser = await User.findOne({ username });
     if (!validUser) {
-      return next(error(404, "User Not Valid"));
+      return next(error(404, 'User Not Valid'));
     }
     if (validUser.username != validEmail.username) {
-      return next(error(404, "User Not Valid"));
+      return next(error(404, 'User Not Valid'));
     }
     const refreshedtoken = jwt.sign(
       { id: validUser._id },
@@ -142,11 +134,8 @@ const refreshToken = async (req, res, next) => {
     const { password: pass, ...rest } = validUser._doc;
     res
       .status(200)
-      .cookie("access_token", refreshedtoken, {
-        path: '/',
-      sameSite: 'None',
-      secure: true,
-      httpOnly: false,
+      .cookie('access_token', refreshedtoken, {
+        httpOnly: true,
         domain: process.env.AUTH_COOKIE_DOMAIN,
       })
       .json(rest);
