d32cc360efc70aa490d42af136eb6ec2bed95b17
punyakriti5 <83115236+punyakriti5@users.noreply.github.com>
2024-02-15 16:18:28 +0530

    cors updated config

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {origin:process.env.CLIENT_URL}
+));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
