b8dab42aab8da264173f87ec9e3a6ac2242f8d42
punyakriti5 <83115236+punyakriti5@users.noreply.github.com>
2024-02-15 16:49:42 +0530

    updating cookie settings

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors(
-  {origin:process.env.CLIENT_URL}
+  {origin:process.env.CLIENT_URL,credentials:true}
 ));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
